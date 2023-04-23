edd88502b03c2fea9d4a66accd4a29cdc3199c60
TRHorse <therunninghorse00@gmail.com>
2023-04-23 06:45:37 +0600

    set initial color-scheme and font families

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "var(--primary)",
+        "primary-focus": "var(--primary-focus)",
+        "primary-content": "var(--primary-content)",
+
+        secondary: "var(--secondary)",
+        "secondary-focus": "var(--secondary-focus)",
+        "secondary-content": "var(--secondary-content)",
+
+        accent: "var(--accent)",
+        "accent-focus": "var(--accent-focus)",
+        "accent-content": "var(--accent-content)",
+
+        neutral: "var(--neutral)",
+        "neutral-focus": "var(--neutral-focus)",
+        "neutral-content": "var(--neutral-content)",
+
+        "base-100": "var(--base-100)",
+        "base-200": "var(--base-200)",
+        "base-300": "var(--base-300)",
+        "base-content": "var(--base-content)",
+
+        info: "var(--info)",
+        "info-content": "var(--info-content)",
+
+        success: "var(--success)",
+        "success-content": "var(--success-content)",
+
+        warning: "var(--warning)",
+        "warning-content": "var(--warning-content)",
+
+        error: "var(--error)",
+        "error-content": "var(--error-content)",
+      },
+
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        "open-sans": ["Open Sans", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+        jost: ["Jost", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
